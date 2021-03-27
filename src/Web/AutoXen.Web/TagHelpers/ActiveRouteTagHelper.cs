6be6ddbf4912ae03bcdd8937d0ba91a6d0a753b2
Simeon Kirilov <Simeon231@users.noreply.github.com>
2021-03-27 19:37:32 +0200

    Active route is now working as expected

--- src/Web/AutoXen.Web/TagHelpers/ActiveRouteTagHelper.cs
+++ src/Web/AutoXen.Web/TagHelpers/ActiveRouteTagHelper.cs
@@ -1,7 +1,5 @@
 ﻿namespace AutoXen.Web.TagHelpers
 {
-    using System;
-    using System.Collections.Generic;
     using System.Linq;
 
     using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,8 +9,6 @@
     [HtmlTargetElement(Attributes = "is-active-route")]
     public class ActiveRouteTagHelper : TagHelper
     {
-        private IDictionary<string, string> routeValues;
-
         [HtmlAttributeName("asp-action")]
         public string Action { get; set; }
 
@@ -24,25 +20,6 @@
 
         [HtmlAttributeName("asp-page")]
         public string Page { get; set; }
-
-        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
-        public IDictionary<string, string> RouteValues
-        {
-            get
-            {
-                if (this.routeValues == null)
-                {
-                    this.routeValues = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-                }
-
-                return this.routeValues;
-            }
-
-            set
-            {
-                this.routeValues = value;
-            }
-        }
 
         [HtmlAttributeNotBound]
         [ViewContext]
@@ -65,38 +42,44 @@
             if (this.ViewContext.RouteData.Values.ContainsKey("Area"))
             {
                 string currentArea = this.ViewContext.RouteData.Values["Area"].ToString();
+
+                if (this.Area == "Identity" && currentArea == this.Area)
+                {
+                    return true;
+                }
+
+                if (string.IsNullOrWhiteSpace(this.Area) || this.Area.ToLower() != currentArea.ToLower())
+                {
+                    return false;
+                }
+            }
+
+            if (this.ViewContext.RouteData.Values.ContainsKey("page"))
+            {
                 string currentPage = this.ViewContext.RouteData.Values["Page"].ToString();
 
-                if (!string.IsNullOrWhiteSpace(this.Area) && this.Area.ToLower() != currentArea.ToLower())
+                if (string.IsNullOrWhiteSpace(this.Page) || this.Page.ToLower() != currentPage.ToLower())
+                {
+                    return false;
+                }
+            }
+
+            if (this.ViewContext.RouteData.Values.ContainsKey("Controller"))
+            {
+                string currentController = this.ViewContext.RouteData.Values["Controller"].ToString();
+                string currentAction = this.ViewContext.RouteData.Values["Action"].ToString();
+
+                if (string.IsNullOrWhiteSpace(this.Controller) || string.IsNullOrWhiteSpace(this.Action))
                 {
                     return false;
                 }
 
-                if (!string.IsNullOrWhiteSpace(this.Page) && this.Page.ToLower() != currentPage.ToLower())
+                if (this.Controller.ToLower() != currentController.ToLower())
                 {
                     return false;
                 }
 
-                return true;
-            }
-
-            string currentController = this.ViewContext.RouteData.Values["Controller"].ToString();
-            string currentAction = this.ViewContext.RouteData.Values["Action"].ToString();
-
-            if (!string.IsNullOrWhiteSpace(this.Controller) && this.Controller.ToLower() != currentController.ToLower())
-            {
-                return false;
-            }
-
-            if (!string.IsNullOrWhiteSpace(this.Action) && this.Action.ToLower() != currentAction.ToLower())
-            {
-                return false;
-            }
-
-            foreach (KeyValuePair<string, string> routeValue in this.RouteValues)
-            {
-                if (!this.ViewContext.RouteData.Values.ContainsKey(routeValue.Key) ||
-                    this.ViewContext.RouteData.Values[routeValue.Key].ToString() != routeValue.Value)
+                if (this.Action.ToLower() != currentAction.ToLower())
                 {
                     return false;
                 }
@@ -110,14 +93,14 @@
             var classAttr = output.Attributes.FirstOrDefault(a => a.Name == "class");
             if (classAttr == null)
             {
-                classAttr = new TagHelperAttribute("class", "active");
+                classAttr = new TagHelperAttribute("class", "active-text-color");
                 output.Attributes.Add(classAttr);
             }
-            else if (classAttr.Value == null || classAttr.Value.ToString().IndexOf("active") < 0)
+            else if (classAttr.Value == null || classAttr.Value.ToString().IndexOf("active-text-color") < 0)
             {
                 output.Attributes.SetAttribute(
                     "class",
-                    classAttr.Value == null ? "active" : classAttr.Value.ToString() + " active");
+                    classAttr.Value == null ? "active-text-color" : classAttr.Value.ToString() + " active-text-color");
             }
         }
     }
