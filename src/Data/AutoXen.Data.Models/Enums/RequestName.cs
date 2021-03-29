e8cdc69d0ec9947d5612300448527f05b9748ef2
Simeon Kirilov <Simeon231@users.noreply.github.com>
2021-03-29 21:20:31 +0300

    Add Bulgarian to Enums

--- src/Data/AutoXen.Data.Models/Enums/RequestName.cs
+++ src/Data/AutoXen.Data.Models/Enums/RequestName.cs
@@ -4,13 +4,15 @@
 
     public enum RequestName
     {
+        [Display(Name = "Workshop")]
         Workshop = 1,
-        [Display(Name="Annual technicall inspection")]
+        [Display(Name= "Annual_technicall_inspection")]
         Annual_technicall_inspection = 2,
-        [Display(Name = "Car wash")]
+        [Display(Name = "CarWash")]
         Car_wash = 3,
+        [Display(Name = "Insurance")]
         Insurance = 4,
-        [Display(Name = "Roadside assistance")]
+        [Display(Name = "Roadside_assistance")]
         Roadside_assistance = 5,
     }
 }
--- src/Data/AutoXen.Data.Models/Enums/TransmissionType.cs
+++ src/Data/AutoXen.Data.Models/Enums/TransmissionType.cs
@@ -1,10 +1,16 @@
 ﻿namespace AutoXen.Data.Models.Enums
 {
+    using System.ComponentModel.DataAnnotations;
+
     public enum TransmissionType
     {
+        [Display(Name = "Unknown")]
         Unknown = 0,
+        [Display(Name = "Automated")]
         Automated = 1,
+        [Display(Name = "Manual")]
         Manual = 2,
+        [Display(Name = "SemiAuto")]
         SemiAuto = 3,
     }
 }
