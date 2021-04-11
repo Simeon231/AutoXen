b270ecd84839817eb384b61f8d3d5a0dc74217aa
Simeon Kirilov <Simeon231@users.noreply.github.com>
2021-04-11 22:04:50 +0300

    Add insurance controller, view and view model

---
+++ src/Web/AutoXen.Web.ViewModels/Insurance/InsuranceRequestViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoXen.Web.ViewModels.Insurance
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class InsuranceRequestViewModel
+    {
+        [Required]
+        public string CarId { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ src/Web/AutoXen.Web/Controllers/InsuranceController.cs
@@ -0,0 +1,23 @@
+﻿namespace AutoXen.Web.Controllers
+{
+    using AutoXen.Web.ViewModels.Insurance;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class InsuranceController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View(new InsuranceRequestViewModel());
+        }
+
+        public ActionResult Details(int id)
+        {
+            return this.View();
+        }
+
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
