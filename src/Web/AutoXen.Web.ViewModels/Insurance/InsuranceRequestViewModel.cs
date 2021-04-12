ecadac035ff8a65140c7d5d90cd09db5ab91ba2f
Simeon Kirilov <Simeon231@users.noreply.github.com>
2021-04-12 14:12:28 +0300

    Add view model resources

--- src/Web/AutoXen.Web.ViewModels/Insurance/InsuranceRequestViewModel.cs
+++ src/Web/AutoXen.Web.ViewModels/Insurance/InsuranceRequestViewModel.cs
@@ -10,20 +10,26 @@
         public string CarId { get; set; }
 
         [Required]
+        [Display(Name = "Company")]
         public int InsurerId { get; set; }
 
         [Required]
+        [Display(Name = "Insurances")]
         public IEnumerable<int> InsuranceIds { get; set; }
 
         [Required]
+        [Display(Name = "StartDate")]
         public DateTime InsurenceStart { get; set; }
 
         [Required]
+        [Display(Name = "EndDate")]
         public DateTime InsuranceEnd { get; set; }
 
         [Required]
+        [Display(Name = "Payments")]
         public byte NumberOfPayments { get; set; }
 
+        [Display(Name = "Message")]
         public string Message { get; set; }
     }
 }
