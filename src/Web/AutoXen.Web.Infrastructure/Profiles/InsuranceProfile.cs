7a5a4cf045a3a093d625275ea33333a9c89933f0
Simeon Kirilov <Simeon231@users.noreply.github.com>
2021-04-12 13:29:35 +0300

    Create InsuranceProfile.cs

---
+++ src/Web/AutoXen.Web.Infrastructure/Profiles/InsuranceProfile.cs
@@ -0,0 +1,21 @@
+﻿namespace AutoXen.Web.Infrastructure.Profiles
+{
+    using AutoMapper;
+    using AutoXen.Data.Models.Insurance;
+    using AutoXen.Web.ViewModels.Insurance;
+
+    public class InsuranceProfile : Profile
+    {
+        public InsuranceProfile()
+        {
+            this.CreateMap<Insurance, InsuranceViewModel>()
+                .ReverseMap();
+
+            this.CreateMap<Insurer, InsurerViewModel>()
+                .ReverseMap();
+
+            this.CreateMap<InsuranceRequest, InsuranceRequestViewModel>()
+                .ReverseMap();
+        }
+    }
+}
