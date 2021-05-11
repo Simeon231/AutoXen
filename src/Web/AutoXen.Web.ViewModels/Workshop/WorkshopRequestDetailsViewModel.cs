6935b83fa207fc948531bbd14d2ab56d90e45fea
Simeon Kirilov <Simeon231@users.noreply.github.com>
2021-05-11 22:16:08 +0300

    Update request information

--- src/Web/AutoXen.Web.ViewModels/Workshop/WorkshopRequestDetailsViewModel.cs
+++ src/Web/AutoXen.Web.ViewModels/Workshop/WorkshopRequestDetailsViewModel.cs
@@ -19,17 +19,6 @@
 
         public AdminRequestInformationViewModel RequestInformation { get; set; }
 
-        [Display(Name = "The car was picked up")]
-        public bool PickedUp { get; set; }
-
-        [Display(Name = "The car services are done")]
-        public bool ServiceFinished { get; set; }
-
-        public DateTime? FinishedOn { get; set; }
-
-        [Display(Name = "The Car was returned")]
-        public bool ReturnedCar { get; set; }
-
         [Display(Name = "Other services")]
         public string OtherServices { get; set; }
 
